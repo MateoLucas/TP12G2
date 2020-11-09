f45599a3738cfe8608759111e4c45f372cd68b36
MateoLucas <mlucas@itba.edu.ar>
2020-11-09 14:54:19 -0300

    TP12 1.1

---
+++ hardware.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   hardware.h
+ * Author: mateo
+ *
+ * Created on November 9, 2020, 2:08 PM
+ */
+
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HARDWARE_H */
+
